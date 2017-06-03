f32260f255b5a490bba502b8acaad2a3d0bbd011
MMz <mmz.source@gmail.com>
2017-06-03 09:56:23 +0200

    add first time-series version

--- src/lead_indicators/core.clj
+++ src/lead_indicators/core.clj
@@ -1,11 +1,30 @@
-(ns lead-indicators.core)
+(ns lead-indicators.core
+  (:require [incanter.core :as incanter]
+            [incanter.stats :as stats]
+            [incanter.charts :as charts]
+            [incanter.optimize :as optimize]
+            [clj-time.format :as time-format]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defn parse-date [date-str]
+  (time-format/parse (time-format/formatter "yyyyMMdd") date-str))
 
-(prn (str "bla" "bla"))
+(def raw-data [["20170531" 3] ["20170530" 1] ["20170529" 4]])
 
-(foo 1)
+(def clean-data (map #(vector (.getMillis (parse-date (first %))) (second %)) raw-data))
+
+(prn clean-data)
+
+(def time-series (incanter/dataset [:date :hours] clean-data))
+
+(def dataset
+  (incanter/dataset [:date :hours] [[7 3][8 2][9 3]]))
+
+(def chart
+  (charts/time-series-plot
+    :date    :hours                             ;; must specify x & y column names of dataset
+    :x-label "Date"
+    :y-label "Hours working with Clojure"
+    :title   "Clojure learning - lead indicator"
+    :data    time-series))
+
 
