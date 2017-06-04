bac6dc9d6807e75c5b61f45f21432336b379b7a4
MMz <mmz.source@gmail.com>
2017-06-04 13:57:28 +0200

    plot cumulative line

--- project.clj
+++ project.clj
@@ -5,5 +5,5 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [clj-time "0.13.0"]
-                 [incanter "1.9.0"]]
+                 [incanter "1.5.7"]] ;; 1.9.1 results in clojure:pom:1.3.0-alpha2 error && 1.9.0 $order is not working
   :main lead-indicators.core)
--- src/lead_indicators/core.clj
+++ src/lead_indicators/core.clj
@@ -5,23 +5,43 @@
             [incanter.charts :as charts]
             [clj-time.format :as time-format]))
 
-(defn parse-date [date-str]
-  (time-format/parse (time-format/formatter "yyyyMMdd") date-str))
+(defn load-file [filename]
+  (io/file (io/resource filename)))
 
-(defn chart [time-series]
-  (charts/time-series-plot
-    :date    :hours                             ;; must specify x & y column names of dataset
-    :x-label "Date"
-    :y-label "Hours working with Clojure"
-    :title   "Clojure learning - lead indicator"
-    :data    time-series))
+(defn load-raw-data [timeseries-file]
+  (edn/read-string (slurp timeseries-file)))
+
+(defn parse-date [date-str]
+  (.getMillis (time-format/parse (time-format/formatter "yyyyMMdd") date-str)))
+
+(defn transform-date-string-col [dataset]
+  (incanter/transform-col dataset :date parse-date))
+
+(defn add-cumulative-column [dataset]
+  (let [cumulative-column (incanter/cumulative-sum (incanter/$ :value dataset))]
+    (incanter/add-column :cumulative cumulative-column dataset)))
+
+(defn time-series-plot [time-series]
+  (let [time-series-plot (charts/time-series-plot
+                           :date    :value            ;; must specify x & y column names of dataset
+                           :x-label "Date"
+                           :y-label "Value"
+                           :points true
+                           :title   "Timeseries of values (red) and cumulative (blue)"
+                           :data    time-series)
+        cumulative-plot (incanter.charts/add-lines time-series-plot
+                                                   (incanter/$ :date time-series)
+                                                   (incanter/$ :cumulative time-series)
+                                                   :points true)]
+    cumulative-plot))
 
 (defn -main []
-  (let [timeseries-file (io/file (io/resource "example"))
-        raw-data (edn/read-string (slurp timeseries-file))
-        clean-data (map #(vector (.getMillis (parse-date (first %))) (second %)) raw-data)
-        time-series (incanter/dataset [:date :hours] clean-data)
-        lead-indicator-chart (chart time-series)]
+  (let [timeseries-file (load-file "clojure")
+        raw-data (load-raw-data timeseries-file)
+        raw-dataset (incanter/dataset [:date :value] raw-data)
+        clean-dataset (transform-date-string-col raw-dataset)
+        ordered-dataset (incanter/$order :date :asc clean-dataset)
+        time-series-dataset (add-cumulative-column ordered-dataset)
+        lead-indicator-chart (time-series-plot time-series-dataset)]
     (incanter/view lead-indicator-chart)))
-
 
